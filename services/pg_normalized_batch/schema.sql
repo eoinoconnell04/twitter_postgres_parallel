47362da0d1663fa973f4e052ea1a48678ecec0d3
Eoin OConnell <eoinoconnell04@gmail.com>
2024-04-11 15:41:32 -0700

    fixed schema

--- services/pg_normalized_batch/schema.sql
+++ services/pg_normalized_batch/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE users (
     id_users BIGINT,
     created_at TIMESTAMPTZ,
     updated_at TIMESTAMPTZ,
-    urls TEXT, 
+    url TEXT, 
     friends_count INTEGER,
     listed_count INTEGER,
     favourites_count INTEGER,
@@ -25,7 +25,7 @@ CREATE TABLE users (
     name TEXT,
     location TEXT,
     description TEXT,
-    withheld_in_countries VARCHAR(2)[],
+    withheld_in_countries VARCHAR(2)[]
 );
 
 /*
@@ -49,7 +49,7 @@ CREATE TABLE tweets (
     state_code VARCHAR(2),
     lang TEXT,
     place_name TEXT,
-    geo geometry,
+    geo geometry
 
     -- NOTE:
     -- We do not have the following foreign keys because they would require us
@@ -62,19 +62,19 @@ CREATE INDEX tweets_index_withheldincountries ON tweets USING gin(withheld_in_co
 
 CREATE TABLE tweet_urls (
     id_tweets BIGINT,
-    urls TEXT, 
+    url TEXT 
 );
 
 
 CREATE TABLE tweet_mentions (
     id_tweets BIGINT,
-    id_users BIGINT,
+    id_users BIGINT
 );
 CREATE INDEX tweet_mentions_index ON tweet_mentions(id_users);
 
 CREATE TABLE tweet_tags (
     id_tweets BIGINT,
-    tag TEXT,
+    tag TEXT
 );
 COMMENT ON TABLE tweet_tags IS 'This table links both hashtags and cashtags';
 CREATE INDEX tweet_tags_index ON tweet_tags(id_tweets);
@@ -82,8 +82,8 @@ CREATE INDEX tweet_tags_index ON tweet_tags(id_tweets);
 
 CREATE TABLE tweet_media (
     id_tweets BIGINT,
-    urls TEXT,
-    type TEXT,
+    url TEXT,
+    type TEXT
 );
 
 /*
